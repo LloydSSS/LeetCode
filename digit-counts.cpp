52db41f981da19b60b00a836badded972c1df7e5
Lloyd <lloydsss@126.com>
2015-06-21 21:25:47 +0800

    Ladder Book: Coding Interviews Optimize Time and Space

---
+++ digit-counts.cpp
@@ -0,0 +1,47 @@
+// Digit Counts
+// http://www.lintcode.com/en/problem/digit-counts/
+// a. 遍历0~n判断每个数字中出现k几次 O(nlogn)
+// b. 判断每一位中k出现了几次，和高位数，低位数以及当前数字都有关系，分情况讨论 O(logn)
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <map>
+#include <string>
+#include <cstdio>
+#include <cstdlib>
+#include <climits>
+#include <cstring>
+using namespace std;
+
+class Solution {
+public:
+    /*
+     * param k : As description.
+     * param n : As description.
+     * return: How many k's between 0 and n.
+     */
+    int digitCounts(int k, int n) {
+        int base = 1;
+        int ans = 0;
+        while (n >= base) {
+            int low = n - (n/base)*base;
+            int high = n/(base*10);
+            int cur = (n/base)%10;
+            if (cur < k)
+                ans += high*base;
+            else if (cur == k)
+                ans += high*base + low + 1;
+            else
+                ans += (high+1) * base;
+            base *= 10;
+        }
+        return ans;
+    }
+};
+
+int main(int argc, char const *argv[]) {
+    Solution sol;
+    return 0;
+}
